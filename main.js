385a1aaa188fbca02fbdaefd084cdb32d31cb221
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-10-11 19:02:41 +0200

    added more functionalities to authenticate users

--- main.js
+++ main.js
@@ -195,3 +195,191 @@ function validateForm3() {
         return false;
     }
 }
+
+
+// crud operations
+
+// Variables
+const nameInput = document.getElementById("name-input");
+const emailInput = document.getElementById("email-input");
+const addBtn = document.getElementById("add-btn");
+const tableBody = document.getElementById("table-body");
+const updateNameInput = document.getElementById("update-name-input");
+const updateEmailInput = document.getElementById("update-email-input");
+const updateFeedbackInput = document.getElementById("update-feedback-input"); // Add the feedback input
+const updateCommentsInput = document.getElementById("update-comments-input"); // Add the comments input
+const updateBtn = document.getElementById("update-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+let users = JSON.parse(localStorage.getItem("users")) || [];
+let currentUserId = null;
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zAZ0-9-]+)*$/;
+
+// Functions
+function renderTable() {
+    tableBody.innerHTML = "";
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      const tr = document.createElement("tr");
+      const idTd = document.createElement("td");
+      const nameTd = document.createElement("td");
+      const emailTd = document.createElement("td");
+      const commentsTd = document.createElement("td"); // Create a new <td> for Comments
+      const feedbackTd = document.createElement("td"); // Create a new <td> for Feedback
+      const actionsTd = document.createElement("td");
+      const editBtn = document.createElement("button");
+      editBtn.className = "edit-btn";
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "delete-btn";
+      idTd.innerText = user.id;
+      nameTd.innerText = user.name;
+      emailTd.innerText = user.email;
+      commentsTd.innerText = user.comments; // Display the "Comments" value
+      feedbackTd.innerText = user.feedback; // Display the "Feedback" value
+      editBtn.innerText = "Edit";
+      deleteBtn.innerText = "Delete";
+      editBtn.addEventListener("click", () => {
+        showUpdateForm(user.id);
+      });
+      deleteBtn.addEventListener("click", () => {
+        deleteUser(user.id);
+      });
+      tr.appendChild(idTd);
+      tr.appendChild(nameTd);
+      tr.appendChild(emailTd);
+      tr.appendChild(commentsTd); // Add "Comments" <td> to the row
+      tr.appendChild(feedbackTd); // Add "Feedback" <td> to the row
+      tr.appendChild(actionsTd);
+      actionsTd.appendChild(editBtn);
+      actionsTd.appendChild(deleteBtn);
+      tableBody.appendChild(tr);
+    }
+  }
+  function addUser() {
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const comments = "Null"; // Default value for comments
+    const feedback = "Null"; // Default value for feedback
+  
+    if (email.match(validRegex)) {
+      if (name && email !== null) {
+        var id = 1;
+        var val = users.map(function (x) {
+          return x.id;
+        }).indexOf(id);
+        while (val != -1) {
+          id++;
+          val = users.map(function (x) {
+            return x.id;
+          }).indexOf(id);
+        }
+  
+        const user = {
+          id: id,
+          name: name,
+          email: email,
+          comments: comments,
+          feedback: feedback,
+        };
+  
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        nameInput.value = "";
+        emailInput.value = "";
+        renderTable();
+      } else {
+        alert("Name is Required");
+      }
+    } else {
+      alert("Invalid email address!");
+    }
+  }
+  // ...
+
+  function showUpdateForm(userId) {
+    const user = users.find((user) => user.id === userId);
+    if (user) {
+      updateNameInput.value = user.name;
+      updateFeedbackInput.value = user.feedback;
+      currentUserId = user.id;
+      updateBtn.addEventListener("click", updateUserFeedback);
+      cancelBtn.addEventListener("click", hideUpdateForm);
+      updateBtn.style.display = "inline-block";
+      cancelBtn.style.display = "inline-block";
+      updateNameInput.style.display = "inline-block";
+      updateFeedbackInput.style.display = "inline-block";
+      document.getElementById("update-container").style.display = "block";
+    }
+  }
+  
+  function updateUserFeedback() {
+    const feedback = updateFeedbackInput.value;
+  
+    const index = users.findIndex((user) => user.id === currentUserId);
+    if (index !== -1) {
+      users[index].feedback = feedback; // Update feedback only
+      localStorage.setItem("users", JSON.stringify(users));
+      renderTable();
+      hideUpdateForm();
+    }
+  }
+  
+  // ...
+  function hideUpdateForm() {
+    updateNameInput.value = "";
+    updateFeedbackInput.value = "";
+    currentUserId = null;
+    updateBtn.removeEventListener("click", updateUserFeedback);
+    cancelBtn.removeEventListener("click", hideUpdateForm);
+    updateBtn.style.display = "none";
+    cancelBtn.style.display = "none";
+    updateNameInput.style.display = "none";
+    updateFeedbackInput.style.display = "none";
+    document.getElementById("update-container").style.display = "none";
+  }
+  
+function deleteUser(userId) {
+  users = users.filter((user) => user.id !== userId);
+  localStorage.setItem("users", JSON.stringify(users));
+  if (users.length == 0){
+    hideUpdateForm();
+  };
+  renderTable();
+}
+
+// Event Listeners
+addBtn.addEventListener("click", addUser);
+
+// Initialize table
+renderTable();
+
+///drop down menu for request grades
+function myFunction(dropdownId) {
+    document.getElementById(dropdownId).classList.toggle("show");
+}
+
+function selectName(name) {
+    var selectedName = document.getElementById("selectedName");
+    selectedName.textContent = name;
+    document.getElementById("myDropdown1").classList.remove("show");
+}
+
+function selectEmail(email) {
+    var selectedEmail = document.getElementById("selectedEmail");
+    selectedEmail.textContent = email;
+    document.getElementById("myDropdown2").classList.remove("show");
+}
+
+// validating student requests for their GeolocationCoordinates
+
+function validateRequest() {
+    var name = document.getElementById('selectedName').textContent;
+    var email = document.getElementById('selectedEmail').textContent;
+    
+    if (name === 'Facilitator Name' || email === 'Facilitator Email') {
+        alert('One or more fields are not selected.');
+    } else {
+        alert('Request sent');
+        // You can submit the form or perform other actions here.
+        window.location.href = 'home.html';
+    }
+}
